e072b162bce34ea7ffb0a803ed1e43319aa78c3c
Maju <mariajuliakohler@gmail.com>
2022-10-11 21:36:34 -0300

    part 5: data

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,9 +1,15 @@
 import React from 'react'; //esse import não é mais necessário para versões do React ^17 (no caso estou usando 18)
+import { MOCK_PROJECTS } from './MockProjects';
 
 //o unico lugar que exige essa expressão import é no arquivo de entrada da aplicação, ou seja, o index.js
 
 function ProjectsPage(){
-    return <h1>Projects</h1>;
+    return (
+    <>
+    <h1>Projects</h1>
+    <pre>{JSON.stringify(MOCK_PROJECTS,null,'')}</pre>
+    </>
+    );
 }
 
 export default ProjectsPage;
